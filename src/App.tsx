6b9c3f68629c33373eb9368f002312ca6ba16207
Chloe Mouret <chloem@theodo.fr>
2023-11-24 18:17:50 +0100

    chore: setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import reactLogo from "./assets/react.svg";
+
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 export const App = (): JSX.Element => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,7 +20,7 @@ export const App = (): JSX.Element => {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
           count is {count}
         </button>
         <p>
@@ -29,7 +31,5 @@ export const App = (): JSX.Element => {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-
+  );
+};

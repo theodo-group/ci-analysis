3b9976c21c3a5cf7ba58c00539a4c2271fc4582c
Chloe Mouret <chloem@theodo.fr>
2023-11-24 15:48:11 +0100

    refactor: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export const App = (): JSX.Element => {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+

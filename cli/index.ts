f07fc582e8ab1c43cf3e030859c4db747d8d58e1
paul-royer <paulr@theodo.fr>
2024-01-12 15:12:37 +0100

    feat: return workflows from main script

--- cli/index.ts
+++ cli/index.ts
@@ -1,20 +1,14 @@
 import parseArgs from "minimist";
+import { GithubClient } from "./client/github/github-client";
+import { Workflow } from "./model/workflow";
+import { ProviderRepositoryInformation } from "./model/provider-repository-information";
+import { AzureClient } from "./client/azure/azure-client";
 
 enum Providers {
   GITHUB = "github",
+  AZURE = "azure",
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface Workflow {
-  id: number;
-  jobs_url: string;
-  status: string;
-  conclusion: string;
-  name: string;
-  created_at: string;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface Job {
   total_count: number;
   workflow: {
@@ -44,7 +38,7 @@ interface Config {
   repo: string;
   token: string;
   max: number;
-  provider: Providers;
+  provider: string;
 }
 
 const getParameters = (): Config => {
@@ -53,35 +47,50 @@ const getParameters = (): Config => {
   >(process.argv, {
     alias: { o: "owner", r: "repo", t: "token", m: "max", p: "provider" },
     string: ["owner", "repo", "token", "max"],
-    default: { provider: Providers.GITHUB },
+    default: { provider: Providers.GITHUB.valueOf() },
   });
-
   if (
     owner !== "" &&
     repo !== "" &&
     token !== "" &&
     max !== "" &&
     !isNaN(parseInt(max)) &&
-    Object.values(Providers).includes(provider)
+    (Object.values(Providers) as string[]).includes(provider)
   ) {
     return { owner, repo, token, max: parseInt(max), provider };
   }
-
+  if (
+    provider !== Providers.GITHUB.valueOf() ||
+    provider !== Providers.AZURE.valueOf()
+  ) {
+    throw new Error("Only github and azure are supported for now");
+  }
   throw new Error("Missing parameter");
 };
 
-const { owner, repo, token, max, provider } = getParameters(process.env);
-
-if (provider !== Providers.GITHUB) {
-  throw new Error("Only github is supported for now");
-}
-
-const GITHUB_BASE_URL = `https://api.github.com/repos/${owner}/${repo}`;
-const DEFAULT_HEADERS = {
-  Authorization: `token ${token}`,
+const { owner, repo, token, max, provider } = getParameters();
+const repositoryInformation: ProviderRepositoryInformation = {
+  token,
+  owner,
+  repo,
 };
+
 const MAX_NUMBER_OF_NEW_WORKFLOWS = max;
 
-const doAll = async () => {};
+const doAll = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  let workflows = new Array<Workflow>();
+  switch (provider) {
+    case Providers.GITHUB:
+      workflows = await new GithubClient().getWorkflows(repositoryInformation);
+      break;
+    case Providers.AZURE:
+      workflows = await new AzureClient().getWorkflows(repositoryInformation);
+      break;
+    default:
+      break;
+  }
+  console.table(workflows);
+};
 
 void doAll();
